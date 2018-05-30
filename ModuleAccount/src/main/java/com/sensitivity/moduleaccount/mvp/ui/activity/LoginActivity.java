41f4988ae95c1ab94a7d8ab1984000fd72281756
Sam <sam.chen@regalscan.com.tw>
2018-05-30 09:26:51 +0800

    Optimize UI for LoginActivity

--- ModuleAccount/src/main/java/com/sensitivity/moduleaccount/mvp/ui/activity/LoginActivity.java
+++ ModuleAccount/src/main/java/com/sensitivity/moduleaccount/mvp/ui/activity/LoginActivity.java
@@ -1,26 +1,44 @@
 package com.sensitivity.moduleaccount.mvp.ui.activity;
 
+import static com.jess.arms.utils.Preconditions.checkNotNull;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.widget.TextView;
 
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
-
+import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
+import com.sensitivity.moduleaccount.R;
 import com.sensitivity.moduleaccount.di.component.DaggerLoginComponent;
 import com.sensitivity.moduleaccount.di.module.LoginModule;
 import com.sensitivity.moduleaccount.mvp.contract.LoginContract;
 import com.sensitivity.moduleaccount.mvp.presenter.LoginPresenter;
 
-import com.sensitivity.moduleaccount.R;
-
-
-import static com.jess.arms.utils.Preconditions.checkNotNull;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class LoginActivity extends BaseActivity<LoginPresenter> implements LoginContract.View {
+
+//    @BindView(R.id.et_email)
+//    TextInputEditText mEtEmail;
+//    @BindView(R.id.et_password)
+//    TextInputEditText mEtPassword;
+//    @BindView(R.id.btn_login)
+//    QMUIRoundButton mBtnLogin;
+//    @BindView(R.id.btn_signUp)
+//    QMUIRoundButton mBtnSignUp;
+//    @BindView(R.id.btn_facebook)
+//    QMUIRoundButton mBtnFacebook;
+//    @BindView(R.id.btn_google)
+//    QMUIRoundButton mBtnGoogle;
+//    @BindView(R.id.tv_forgotPassword)
+//    TextView mTvForgotPassword;
 
     @Override
     public void setupActivityComponent(@NonNull AppComponent appComponent) {
@@ -67,5 +85,12 @@ public class LoginActivity extends BaseActivity<LoginPresenter> implements Login
     @Override
     public void killMyself() {
         finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
